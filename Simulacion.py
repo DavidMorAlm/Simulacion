64ebe1748bffc7fe10b8b4a710907c98432304cb
davidmoralm <david.moralm02@gmail.com>
2022-10-10 00:12:05 -0500

    Ejercicio dados listo

--- Simulacion.py
+++ Simulacion.py
@@ -2,12 +2,13 @@ from Pseudo import Pseudo
 import math
 
 
+# Parametros iniciales para generar los numeros pseudo-aleatorios
 x0 = 6
 g = 13
 k = 15
 c = 8191
 alfa = 0.05
-
+# Se generan los pseudo y se les aplican cada una de las pruebas correspondientes
 print("")
 n = Pseudo(x0, g, k, c)
 n.gLineal()
